21bc60348450a6601e15c212b939589a9a94f66c
AsadImtiazRana <asadimtiaz711@gmail.com>
2020-08-10 12:38:05 +0500

    Color Picker removed

--- javascript/pickr.js
+++
@@ -1,5 +0,0 @@
-
-document.querySelector(".color-pickr").addEventListener("input", e => {
-    inputColor.setAttribute("value",document.querySelector(".color-pickr").value);
-    document.querySelector(".color-picker").setAttribute("data-color", document.querySelector(".color-pickr").getAttribute("value"));
-});
